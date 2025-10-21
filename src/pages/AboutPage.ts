61e2e6ec6916d2671e057919a5d0bd03f6b286c7
alienpingu <samuele.bertocco@gmail.com>
2025-10-21 12:53:25 +0200

    chore: write about

--- src/pages/AboutPage.ts
+++ src/pages/AboutPage.ts
@@ -4,8 +4,14 @@ export class AboutPage extends BaseComponent {
   render(): string {
     return `
       <h1>About</h1>
-      <p>Lorem ipsum</p>
-    `;
+      <p>Every now and then, I find myself building the same simple search engine for the CocktailDB API. As I keep learning new things, this time I decided to develop it without using any frameworks. My goal is to create a minimal progressive web app (PWA) featuring:</p>
+      <ul>
+        <li>Virtual routing</li>
+        <li>Global state management</li>
+        <li>Abstract components</li>
+        <li>Minimal use of css</li>
+      </ul>
+      `;
   }
 
   protected bindEvents(): void {}
